8b78fc925061172b9d8ba7f21d0b9d5c381f9064
sahijwaraich13 <sahijwaraich05@gmail.com>
2025-10-04 12:39:18 -0700

    Create Logger.java

---
+++ src/Logger.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+    public static void log(String event) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println("[" + timestamp + "] " + event);
+    }
+}
