d7242773ce8ecf0802f6630a75245889d4d6fcae
sahijwaraich13 <sahijwaraich05@gmail.com>
2025-10-05 23:37:58 -0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-
-# Compile Java files
+# scripts/run.sh
 echo "Compiling Java files..."
-mkdir -p out
-javac src/*.java -d out
+mkdir -p ../out
+javac ../src/*.java -d ../out
 
-# Run the Main program
-echo "Starting the entire system via Main..."
-java -cp out Main
+echo "Starting all components..."
+
+# Start Monitor
+osascript -e 'tell app "Terminal" to do script "cd ~/Comp-370-Mini-Project-1 && java -cp out MonitorMain"'
+sleep 1
+
+# Start Primary Server
+osascript -e 'tell app "Terminal" to do script "cd ~/Comp-370-Mini-Project-1 && java -cp out Main primary"'
+sleep 1
+
+# Start Backup 1
+osascript -e 'tell app "Terminal" to do script "cd ~/Comp-370-Mini-Project-1 && java -cp out Main backup1"'
+sleep 1
+
+# Start Backup 2
+osascript -e 'tell app "Terminal" to do script "cd ~/Comp-370-Mini-Project-1 && java -cp out Main backup2"'
+sleep 1
+
+# Start Admin Interface
+osascript -e 'tell app "Terminal" to do script "cd ~/Comp-370-Mini-Project-1 && java -cp out AdminInterfaceMain"'
+sleep 1
+
+echo "All components started!"
