0fde1c6f86f9546f8762e1242c4094fcff9d1049
sahijwaraich13 <sahijwaraich05@gmail.com>
2025-10-06 13:53:18 -0700

    Update delay-heartbeat.sh

--- scripts/delay-heartbeat.sh
+++ scripts/delay-heartbeat.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-PID_FILE="scripts/.pids"
-
-PRIMARY_PID=$(head -n 1 $PID_FILE) # first line is PrimaryServer
-
-if [ -z "$PRIMARY_PID" ]; then
-    echo "Primary server PID not found."
-    exit 1
+BACKUP2_PID_FILE="scripts/.pids/backup2.pid"
+if [ -f "$BACKUP2_PID_FILE" ]; then
+    echo "Delaying heartbeat for Backup Server 2..."
+    kill -STOP $(cat "$BACKUP2_PID_FILE")
+    sleep 5
+    kill -CONT $(cat "$BACKUP2_PID_FILE")
+    echo "Backup Server 2 heartbeat resumed."
+else
+    echo "Backup Server 2 is not running."
 fi
-
-echo "Simulating heartbeat delay..."
-kill -STOP $PRIMARY_PID
-sleep 5
-kill -CONT $PRIMARY_PID
-echo "Primary server heartbeat resumed."
-
 
 
