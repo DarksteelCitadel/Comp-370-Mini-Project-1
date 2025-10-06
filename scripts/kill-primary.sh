35e8b5a9633154ddeca02199c484fb4d8f7ba26e
sahijwaraich13 <sahijwaraich05@gmail.com>
2025-10-06 13:43:45 -0700

    Update kill-primary.sh

--- scripts/kill-primary.sh
+++ scripts/kill-primary.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PID_FILE="scripts/.pids"
 
-PRIMARY_PID=$(grep "PrimaryServer" $PID_FILE | cut -d':' -f2)
+PRIMARY_PID=$(head -n 1 $PID_FILE) # first line is PrimaryServer
 
 if [ -z "$PRIMARY_PID" ]; then
-    echo "Primary server is not running."
+    echo "Primary server PID not found."
     exit 1
 fi
 
 kill -9 $PRIMARY_PID
-echo "Primary server killed (PID $PRIMARY_PID)."
+echo "Primary server killed."
