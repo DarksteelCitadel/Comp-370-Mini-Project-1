d54f91aa275753212e160962b6a109f2f010e91d
sahijwaraich13 <sahijwaraich05@gmail.com>
2025-10-05 23:14:32 -0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Compile Java files
-echo "Compiling Java files..."
+# Create output folder
 mkdir -p out
+
+# Compile all Java files
+echo "Compiling Java files..."
 javac src/*.java -d out
 
-# Run the Main program which starts everything
-echo "Starting the SRMS simulation..."
+# Run the whole system from Main.java
+echo "Starting system (Monitor, PrimaryServer, BackupServers, AdminInterface)..."
 java -cp out Main
-
-
