71b700522fca7319ee7fcd747ee30c12d9fe11ca
sahijwaraich13 <sahijwaraich05@gmail.com>
2025-10-06 13:03:18 -0700

    Update kill-primary.sh

--- scripts/kill-primary.sh
+++ scripts/kill-primary.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Kill the Java process running the primary server
-PRIMARY_PID=$(jps -l | grep -i "PrimaryServer 1" | awk '{print $1}')
+# kill-primary.sh - kills the JVM hosting the current PrimaryServer
+
+# Find the PID of the running PrimaryServer
+PRIMARY_PID=$(jps -l | grep -i "PrimaryServer" | awk '{print $1}')
 
 if [ -z "$PRIMARY_PID" ]; then
     echo "Primary server is not running."
     exit 1
 fi
 
+# Kill the primary server
 kill -9 $PRIMARY_PID
-echo "Primary server killed."
+echo "Primary server with PID $PRIMARY_PID killed."
