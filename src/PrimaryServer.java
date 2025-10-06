2e6f9df8b82a2f2028d93742af19fcf5e474ecfa
sahijwaraich13 <sahijwaraich05@gmail.com>
2025-10-06 01:02:48 -0700

    for step 10 needed to add main funcation

--- src/PrimaryServer.java
+++ src/PrimaryServer.java
@@ -18,4 +18,27 @@ public class PrimaryServer extends ServerProcess {
         Logger.log("PrimaryServer " + id + " handling request: " + request);
         return "Response from PrimaryServer " + id + ": " + request;
     }
+
+    public static void main(String[] args) {
+        int id = 1;
+        int port = 6000;
+        if (args.length >= 2) {
+            try {
+                id = Integer.parseInt(args[0]);
+                port = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid arguments, using defaults id=1 port=6000");
+            }
+        }
+        PrimaryServer server = new PrimaryServer(id, port);
+        try {
+            server.start();
+            // Keep running
+            while (true) {
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
